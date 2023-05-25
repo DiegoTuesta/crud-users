a9b6e4e0845c4e982ee5f395a5b5ee7d18758600
Diego Tuesta <diego.productividad@gmail.com>
2023-05-24 23:20:54 -0500

    Project Final

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -7,7 +7,7 @@ const Message = ({info, confirm}) => {
         <h2 style={{ color: info.type === 'c' ? 'green' : info.type === 'u' ? 'orange' : '#db3333' }} >{ info.type === 'c' ? 'New User' : info.type === 'u' ? 'Update User' : info.type === 'dq' ? 'Delete' : 'Delete user' }</h2>
         <p>{ info.type === 'c' ? `User ${info.data} add!` : info.type === 'u' ? `User ${info.data} update!` : info.type === 'dq' ?  `Are you sure to delete ${info.data}? ` :  `User ${info.data} delete!` }</p>
         </div>
-       {info.type === 'dq' &&  <div className='message-footer'><button onClick={confirm}>Confirm</button></div>}
+       {info.type === 'dq' &&  <div className='message-footer'><button className='btn' onClick={confirm}>Confirm</button></div>}
        
     </div>
   )
